b148c9dc3c4430bf518239b3348a26378bf90c54
DennisTarik <dm499555@gmail.com>
2021-10-20 15:44:27 +0200

    ⚡ moved create characters into main.js

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
-import './style.css'
-import { createHeaderComponent } from './lib/headerComponent';
-import { createMainComponent } from './lib/mainComponent';
-import { /* createFooterComponent */ } from './lib/footerComponent';
+import "./style.css";
+import { createHeaderComponent } from "./lib/headerComponent";
+import { createMainComponent } from "./lib/mainComponent";
+import /* createFooterComponent */ "./lib/footerComponent";
 
 function renderApp() {
-  const appElement = document.querySelector('#app');
+  const appElement = document.querySelector("#app");
 
   const headerComponent = createHeaderComponent();
 
-  const mainComponent = createMainComponent();
+  const characters = [
+    {
+      name: "Pawnshop Clerk",
+      imgSrc: "https://rickandmortyapi.com/api/character/avatar/258.jpeg",
+      status: "Alive",
+      race: "Alien",
+      lastKnownLocation: "Pawn Shop Planet",
+      firstSeenIn: "Raising Gazorpazorp",
+    },
+    {
+      name: "Pencilvester",
+      imgSrc: "https://rickandmortyapi.com/api/character/avatar/259.jpeg",
+      status: "Dead",
+      race: "Alien",
+      lastKnownLocation: "Earth (Replacement Dimension)",
+      firstSeenIn: "Total Rickall",
+    },
+    {
+      name: "Dr. Xenon Bloom",
+      imgSrc: "https://rickandmortyapi.com/api/character/avatar/108.jpeg",
+      status: "Dead",
+      race: "Humanoid",
+      lastKnownLocation: "Anatomy Park",
+      firstSeenIn: "Anatomy Park",
+    },
+  ];
 
-/*   const footerComponent = createFooterComponent(); */
+  const mainComponent = createMainComponent(characters);
 
-  appElement.append(headerComponent, mainComponent, /* footerComponent */);
+  /*   const footerComponent = createFooterComponent(); */
+
+  appElement.append(headerComponent, mainComponent /* footerComponent */);
 }
 
 renderApp();
